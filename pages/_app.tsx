3ac8b7ad941667eb42eb7a819f590b6d8a88d45a
Jorge Roldán <icabina@live.com>
2022-06-13 18:33:52 -0500

    m

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { lightTheme } from '../themes'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SWRConfig } from 'swr';
+
+import { lightTheme } from '../themes';
+import { UiProvider } from '../context';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+  return (
+    <SWRConfig 
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <UiProvider isMenuOpen={false}>
+        <ThemeProvider theme={ lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
+    </SWRConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
-import { Typography } from '@mui/material'
-import type { NextPage } from 'next'
-import { ShopLayout } from '../components/layouts'
-import { ProductList } from '../components/products'
-import {initialData} from '../database/products'
+import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+import { ShopLayout } from '../components/layouts';
+
+import { ProductList } from '../components/products';
+import { useProducts } from '../hooks';
+
+import { FullScreenLoading } from '../components/ui';
 
 
-const Home = () => {
+const HomePage: NextPage = () => {
+
+
+  const { products, isLoading } = useProducts('/products');
+
+
   return (
-  <ShopLayout title={'Next Shop'} pageDescription={'Built with NextJS'}>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
+        <Typography variant='h1' component='h1'>Tienda</Typography>
+        <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-<Typography variant='h1' component='h1'>Tienda</Typography>
-<Typography variant='h2' sx={{marginBottom: 1}}>Todos los productos</Typography>
+        {
+          isLoading
+            ? <FullScreenLoading />
+            : <ProductList products={ products } />
+        }
 
-      <ProductList products={initialData.products as any}/>
+        
+    
 
-
-  </ShopLayout>
+    </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
