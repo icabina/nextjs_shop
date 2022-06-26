fad963ee744665ca5427d61cedca3db46fe63671
Jorge Roldán <icabina@live.com>
2022-06-25 19:04:43 -0500

    m

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,7 @@
 
-
 export * from './cart';
+export * from './dashboard';
+export * from './order';
 export * from './products';
 export * from './user';
-export * from './order';
 export * as IPaypal from './paypal';
